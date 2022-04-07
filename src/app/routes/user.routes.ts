87f49a4cda34c71bc1035aa89c09f589b1a93995
Caleby <calebysoares@gmail.com>
2022-04-07 20:34:03 -0300

    commit

--- src/app/routes/user.routes.ts
+++ src/app/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import authMIddleware from '../middlewares/auth';
 
 const userRoutes = Router();
 
-userRoutes.post('/', UserController.store);
 userRoutes.get('/', UserController.index);
+
+userRoutes.use(authMIddleware);
+userRoutes.post('/', UserController.store);
 userRoutes.get('/:id', UserController.show);
 userRoutes.put('/:id', UserController.update);
 userRoutes.delete('/:id', UserController.delete);
